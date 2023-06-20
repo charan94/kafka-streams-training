0ce512385a27e91118151311a78caf0e45b36816
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2023-06-20 07:44:04 -0400

    INITIAL COMMIT

---
+++ src/main/java/com/itcratstraining/KafkaStreamsTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.itcratstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itcratstraining/KafkaStreamsTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcratstraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
